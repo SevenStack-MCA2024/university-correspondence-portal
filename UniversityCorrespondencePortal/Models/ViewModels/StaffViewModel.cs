93c263384857905bdf0c41c449f4eaaf0d565b5f
vishal_Sonone <vishaljsonone@gmail.com>
2025-08-03 00:41:38 +0530

    Implement admin ui

---
+++ UniversityCorrespondencePortal/Models/ViewModels/StaffViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCorrespondencePortal.Models.ViewModels
+{
+    public class StaffViewModel
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Designation { get; set; }
+        public string Departments { get; set; }
+    }
+}
