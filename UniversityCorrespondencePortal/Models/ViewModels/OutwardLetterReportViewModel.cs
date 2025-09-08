9d1e95501de90e18e2564cbf94871e85775d1672
Rohitmahajan004 <rohitmahajan123bca@gmail.com>
2025-09-08 21:56:42 +0530

    add report page in clerk panel

---
+++ UniversityCorrespondencePortal/Models/ViewModels/OutwardLetterReportViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityCorrespondencePortal.Models.ViewModels
+{
+    public class OutwardLetterReportViewModel
+    {
+        public string LetterNo { get; set; }
+        public DateTime? Date { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public string ReceiverDepartment { get; set; }
+        public string Subject { get; set; }
+    }
+}
